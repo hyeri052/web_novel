e6f9b71d57787e1249b4abc7812061e2c100cca7
hyeri052 <hyeri20030609@gmail.com>
2025-12-30 02:34:00 +0900

    Add files via upload

---
+++ next.config.js
@@ -0,0 +1,11 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    output: 'export',
+    images:{
+      unoptimized: true,
+    },
+    basePath: '/web_novel',      // ⭐ 레포지토리 이름
+    assetPrefix: '/web_novel/',  // ⭐ 슬래시 포함
+  };
+  
+  module.exports = nextConfig;
